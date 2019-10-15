cf3556abdba5eb5289570dea1e11db9989c4d0c9
twesleyb <tyler.w.bradshaw@duke.edu>
2019-10-15 09:46:27 -0400

    lazy git commit

---
+++ R/getHomoloGene.R
@@ -0,0 +1,76 @@
+# Downloading NCBI homology database. And mapping proteins to a species of interst.
+
+# Download and load NCBI homology gene data.
+getHomoloGene <- function(hitpredict,taxid,downloads=getwd(),keepdata=FALSE){
+
+	url <- "ftp://ftp.ncbi.nih.gov/pub/HomoloGene/current/homologene.data"
+	destfile <- file.path(downloads, "homologene.data")
+	if (!file.exists(destfile)) {
+		message("Downloading NCBI HomoloGene data...")
+		download.file(url, destfile)
+		homologene <- data.table::fread(destfile, header = FALSE)
+	} else {
+		message("Loading NCBI HomoloGene data from file!")
+		homologene <- data.table::fread(destfile, header = FALSE)
+	}
+	# Remove raw data.
+	if (keepdata==FALSE) {
+		unlink(destfile)
+	}
+
+	# Fix column names.
+	# Gene ID is a genes organism specific Entrez ID.
+	# HID is the genes homology id.
+	homologene <- homologene %>% rename_all(list(~ c("HID", "TaxonomyID", "GeneID",
+							 "GeneSymbol","ProteinGI","ProteinAccession")))
+
+	# Use HomoloGene to create homology map.
+	homology_map <- as.list(homologene$HID)
+	names(homology_map) <- homologene$GeneID
+
+	# Map EntrezA to homology ID.
+	hidA <- homology_map[hitpredict$EntrezA]
+	hidA[seq(1:length(hidA))[unlist(lapply(hidA,is.null))]] <- NA
+	hitpredict$HIDA <- unlist(hidA)
+	# Map EntrezB to homology ID.
+	hidB <- homology_map[hitpredict$EntrezB]
+	hidB[seq(1:length(hidB))[unlist(lapply(hidB,is.null))]] <- NA
+	hitpredict$HIDB <- unlist(hidB)
+
+	# Subset homologene data, keep all genes from your species of interest.
+	homologene <- homologene %>% filter(TaxonomyID==taxid)
+
+	# Taxonomy info.
+	source("annotationDBs.R")
+	osDB <- unlist(annotationDBs[sapply(annotationDBs,"[",1) == taxid])
+	names(osDB) <- sapply(strsplit(names(osDB),"\\."),"[",2)
+	organism <- osDB["alias"]
+
+	# Get entrez associated with these HIDs.
+	hitpredict <- dplyr::mutate(hitpredict,
+				    osEntrezA=homologene$GeneID[match(hitpredict$HIDA,homologene$HID)])
+	hitpredict <- dplyr::mutate(hitpredict,
+				    osEntrezB=homologene$GeneID[match(hitpredict$HIDB,homologene$HID)])
+
+	# Status report.
+	is_missing <- is.na(hitpredict$osEntrezA) | is.na(hitpredict$osEntrezB) 
+	n <- length(is_missing)
+	message(paste0(round(100*sum(is_missing)/n,2), 
+		       "% of HitPredict interactions were mapped to homologous genes in ", 
+		       organism,"."))
+
+	# Remove rows with NA.
+	hitpredict <- dplyr::filter(hitpredict, !is_missing)
+
+	# Status report.
+	ntaxid <- length(unique(c(hitpredict$Interactor_A_Taxonomy,hitpredict$Interactor_B_Taxonomy)))
+	nppis <- format(dim(hitpredict)[1],nsmal=0,big.mark=",")
+	ngenes <- format(length(unique(c(hitpredict$osEntrezA,hitpredict$osEntrezB))),
+			 nsmall=0,big.mark=",")
+	message(paste0(nppis," protein-protein interactions among ", ngenes, 
+		       " ", organism, " genes were compiled from ", ntaxid, " species."))
+
+	# Return data with genes mapped to gene specific entrez.
+	return(hitpredict)
+}
+
--- README.md
+++ README.md
@@ -1,21 +1,21 @@
 # getPPIs
-Compiling experimentally identified protein protein interactions (PPIs) from 
-the __HitPredict__ database [^1].
+Compiling experimentally identified protein-protein interactions (PPIs) from 
+the __HitPredict__ database.
 
-# Motivation
+## Motivation
 This package facilitates downloading and compiling PPIs from the HitPredict database.
-Of the xxx unique proteins in the HitPredict database, xxx (yyy%) were successfully 
-mapped to Entrez IDs.
-
-In total, this consisted of XXX proteins and YYY protein-protein interactions.
 
 ## HitPredict
 [HitPredict](http://hintdb.hgc.jp/htp/)
 > Protein-protein interactions from IntAct, BioGRID, HPRD, MINT and DIP are combined, annotated and scored. The reliability score is calculated based on the experimental details of each interaction and the sequence, structure and functional annotations of the interacting proteins
 
-[^1] For more information, please see authors' original publications:
+For more information, please see authors' original publications:
 * Lopez _et al.,_ [Database 2015](https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4691340/)
 * Patil _et al.,_ [Nucleic Acids Res. 2011](https://www.ncbi.nlm.nih.gov/pubmed/20947562)
 * Patil and Nakamura [BMC Bioinformatics 2005](https://www.ncbi.nlm.nih.gov/pubmed/15833142)
 
+## Usage
+Download the getPPIs R package.
+```
+```
 
