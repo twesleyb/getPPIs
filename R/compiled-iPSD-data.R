59f53d7e8d9b914a3bd5c342bf1467b1fb516896
twesleyb <tyler.w.bradshaw@duke.edu>
2019-10-15 13:28:24 -0400

    lazy git commit

---
+++ R/compiled-iPSD-data.R
@@ -0,0 +1,19 @@
+#' compiled iPSD proteome
+#' 
+#' A proteome of inhibitory synapses compile from several studies.
+#'
+#' @docType data
+#'
+#' @usage data(compiled-iPSD)
+#'
+#' @format none
+#'
+#' @keywords datasets
+#'
+#' @references Uezu et al. (2013) Science 353(6304):1123-9.
+#' (\href{https://www.ncbi.nlm.nih.gov/pubmed/27609886}{PubMed})
+#'
+#' @source none
+#'
+#' @examples
+#' data(compiled-iPSD)
---
+++ R/synaptosome-data.R
@@ -0,0 +1,18 @@
+#' Synaptosome proteome
+#' 
+#' A synaptosome proteome.
+#'
+#' @docType data
+#'
+#' @usage data(synaptosome)
+#'
+#' @format none
+#'
+#' @keywords datasets
+#'
+#' @references none
+#'
+#' @source none
+#'
+#' @examples
+#' data(synaptosome)
