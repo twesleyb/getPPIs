34cbce1aa3bb5ccf4d4ea7af33190a033b1e2adf
twesleyb <tyler.w.bradshaw@duke.edu>
2019-10-17 15:33:02 -0400

    update

---
+++ example.R
@@ -0,0 +1,84 @@
+#------------------------------------------------------------------------------
+## Installation.
+#------------------------------------------------------------------------------
+
+# Install from github.
+devtools::install_github("twesleyb/getPPIs")
+
+#------------------------------------------------------------------------------
+## Download, compile, annotate, and subset PPIs in a single step.
+#------------------------------------------------------------------------------
+
+# Load the library.
+library(getPPIs)
+
+# Load an example dataset.
+data(compiled_iPSD)
+
+# Get PPIs amongst iPSD proteins (mouse).
+# Keep all PPIs that are homologous to mouse genes.
+ppis <- getPPIs(organism="HitPredict", mygenes=compiled_iPSD, taxid=10090)
+
+# Example: Building a ppi network with igraph.
+library(org.Mm.eg.db)
+
+sif <- ppis %>% dplyr::select(osEntrezA, osEntrezB)
+
+nodes <- unique(c(sif$osEntrezA, sif$osEntrezB))
+
+symbols <- AnnotationDbi::mapIds(org.Mm.eg.db,
+				 keys = as.character(nodes),
+				 column = "SYMBOL",
+				 keytype = "ENTREZID",
+				 multiVals = "first")
+noa <- data.table::data.table(node = nodes, symbol = symbols)
+
+# Check.
+if (sum(is.na(symbols)) == 0) {
+	message("All node Entrez IDs mapped to gene symbols!")
+}
+
+# Build igraph object.
+g <- igraph::graph_from_data_frame(sif, directed = FALSE, vertices = nodes)
+
+# Insure that the graph is simple.
+g <- igraph::simplify(g)
+
+# Basic graph properties.
+length(V(g))
+length(E(g))
+
+# You can save these files to work with them in Cystoscape.
+data.table::fwrite(noa,"noa.csv")
+data.table::fwrite(sif,"sif.csv")
+
+# Alternatively, check out the RCy3 package to interface with Cytscape from R!
+
+#------------------------------------------------------------------------------
+## Using the prebuild mouse Interactome.
+#------------------------------------------------------------------------------
+
+data(musInteractome)
+
+
+
+#------------------------------------------------------------------------------
+## Building an interactome from scratch.
+#------------------------------------------------------------------------------
+
+# Load the library.
+library(getPPIs)
+
+# Download all PPIs in the HitPredict database.
+# This function will also annotate Genes with Entrez IDs, and discard those 
+# which are not mapped to Entrez..
+hitpredict <- getHitPredict(organism="HitPredict")
+
+# Map genes to mouse homologs.
+hitpredict <- getHomoloGene(hitpredict, taxid = 10090)
+
+# Annotate hitpredict data with more descriptive method names.
+# Keep all experimental evidence, no confidence score cutoff.
+hitpredict <- getMethods(hitpredict)
+
+
