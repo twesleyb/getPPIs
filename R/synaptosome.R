6d07e9d053364eba51b06fea653d9a2b26f5ac83
twesleyb <tyler.w.bradshaw@duke.edu>
2019-10-17 10:34:27 -0400

    updating datasets

---
+++ R/synaptosome.R
@@ -0,0 +1,19 @@
+#' Synaptosome proteome
+#'
+#' A synaptosome proteome.
+#'
+#' @docType data
+#'
+#' @usage data(synaptosome)
+#'
+#' @format atomic vector (integers) Entrez IDs
+#'
+#' @keywords datasets
+#'
+#' @references none
+#'
+#' @source none
+#'
+#' @examples
+#' data(synaptosome)
+"synaptosome"
