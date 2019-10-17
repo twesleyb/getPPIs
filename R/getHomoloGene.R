7102273540182f81008ec63cd8103824637b81ba
twesleyb <tyler.w.bradshaw@duke.edu>
2019-10-17 12:24:22 -0400

    lazy git commit

--- R/getHomoloGene.R
+++ R/getHomoloGene.R
@@ -7,10 +7,6 @@
 #'
 #' @param taxid (integer) - taxonomic identifier for species of interest
 #'
-#' @param downloads (character) - directory for downloaded data
-#'
-#' @param keepdata (logical) - should downloaded data be kept or removed?
-#'
 #' @return none
 #'
 #' @author Tyler W Bradshaw, \email{tyler.w.bradshaw@duke.edu}
@@ -23,26 +19,19 @@
 #'
 #' @examples
 #' getHomoloGene(hitpredict, taxid = 10090)
-getHomoloGene <- function(hitpredict, taxid, downloads = getwd(), keepdata = FALSE) {
+getHomoloGene <- function(hitpredict, taxid) {
   suppressMessages({
     require(getPPIs)
   })
   # Download and load NCBI homology gene data.
+  downloads <- getwd()
   url <- "ftp://ftp.ncbi.nih.gov/pub/HomoloGene/current/homologene.data"
   destfile <- file.path(downloads, "homologene.data")
-  if (!file.exists(destfile)) {
-    message("Downloading NCBI HomoloGene data...")
-    download.file(url, destfile)
-    homologene <- data.table::fread(destfile, header = FALSE)
-  } else {
-    message("Loading NCBI HomoloGene data from file!")
-    homologene <- data.table::fread(destfile, header = FALSE)
-  }
+  message("Downloading NCBI HomoloGene data...")
+  download.file(url, destfile)
+  homologene <- data.table::fread(destfile, header = FALSE)
   # Remove raw data.
-  if (keepdata == FALSE) {
-    unlink(destfile)
-  }
-
+  unlink(destfile)
   # Fix column names.
   # Gene ID is a genes organism specific Entrez ID.
   # HID is the genes homology id.
@@ -50,11 +39,9 @@ getHomoloGene <- function(hitpredict, taxid, downloads = getwd(), keepdata = FAL
     "HID", "TaxonomyID", "GeneID",
     "GeneSymbol", "ProteinGI", "ProteinAccession"
   )))
-
   # Use HomoloGene to create homology map.
   homology_map <- as.list(homologene$HID)
   names(homology_map) <- homologene$GeneID
-
   # Map EntrezA to homology ID.
   hidA <- homology_map[hitpredict$EntrezA]
   hidA[seq(1:length(hidA))[unlist(lapply(hidA, is.null))]] <- NA
@@ -63,16 +50,13 @@ getHomoloGene <- function(hitpredict, taxid, downloads = getwd(), keepdata = FAL
   hidB <- homology_map[hitpredict$EntrezB]
   hidB[seq(1:length(hidB))[unlist(lapply(hidB, is.null))]] <- NA
   hitpredict$HIDB <- unlist(hidB)
-
   # Subset homologene data, keep all genes from your species of interest.
   homologene <- homologene %>% filter(TaxonomyID == taxid)
-
   # Taxonomy info.
   annotationDBs <- getDBs()
   osDB <- unlist(annotationDBs[sapply(annotationDBs, "[", 1) == taxid])
   names(osDB) <- sapply(strsplit(names(osDB), "\\."), "[", 2)
   organism <- osDB["alias"]
-
   # Get entrez associated with these HIDs.
   hitpredict <- dplyr::mutate(hitpredict,
     osEntrezA = homologene$GeneID[match(hitpredict$HIDA, homologene$HID)]
@@ -80,7 +64,6 @@ getHomoloGene <- function(hitpredict, taxid, downloads = getwd(), keepdata = FAL
   hitpredict <- dplyr::mutate(hitpredict,
     osEntrezB = homologene$GeneID[match(hitpredict$HIDB, homologene$HID)]
   )
-
   # Status report.
   is_missing <- is.na(hitpredict$osEntrezA) | is.na(hitpredict$osEntrezB)
   n <- length(is_missing)
@@ -89,10 +72,8 @@ getHomoloGene <- function(hitpredict, taxid, downloads = getwd(), keepdata = FAL
     "% of HitPredict interactions were mapped to homologous genes in ",
     organism, "."
   ))
-
   # Remove rows with NA.
   hitpredict <- dplyr::filter(hitpredict, !is_missing)
-
   # Status report.
   ntaxid <- length(unique(c(hitpredict$Interactor_A_Taxonomy, hitpredict$Interactor_B_Taxonomy)))
   nppis <- format(dim(hitpredict)[1], nsmal = 0, big.mark = ",")
@@ -103,7 +84,6 @@ getHomoloGene <- function(hitpredict, taxid, downloads = getwd(), keepdata = FAL
     nppis, " protein-protein interactions among ", ngenes,
     " ", organism, " genes were compiled from ", ntaxid, " species."
   ))
-
   # Return data with genes mapped to gene specific entrez.
   return(hitpredict)
 }
