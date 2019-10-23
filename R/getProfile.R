fe0ad33d926f2ce5e199bf2f647f6c2b1beda540
twesleyb <tyler.w.bradshaw@duke.edu>
2019-10-23 13:48:18 -0400

    lazy git commit

--- R/getProfile.R
+++ R/getProfile.R
@@ -2,7 +2,12 @@
 #'
 #' Partition the network with the Leiden algorithm.
 #'
-#' @param none
+#' @param g - igraph object
+#'
+#' @param partition_type - Leiden algorithm method to be used.
+#'
+#' @param nsteps - number of resolutions to be analyzed. If 1 then, 
+#' the native resolution (r=0) will be used.
 #'
 #' @return none
 #'
@@ -19,12 +24,19 @@
 #' @examples
 #' getProfile()
 getProfile <- function(g, partition_type, nsteps = 1, ...) {
-  # Imports.
-  suppressPackageStartupMessages({
-    require(leiden)
-  })
-  resolution <- seq(0, 1, length.out = nsteps)
-  profile <- list()
+       	# Imports.
+	suppressPackageStartupMessages({
+		require(leiden)
+	})
+	# Initialize progress bar.
+	if (i==1) { 
+		message(paste("Examining optimal partitioning of the graph at",nsteps,"resolutions."))
+		pb <- txtProgressBar(min = 0, max = nsteps, initial = 0) 
+	}
+	setTxtProgressBar(pb,i)
+	# Loop through resolutions and perform leiden algorithm clustering using specified method.
+	resolution <- seq(0, 1, length.out = nsteps)
+       	profile <- list()
   for (i in 1:nsteps) {
     partition <- leiden(g,
       partition_type,
