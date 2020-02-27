cfe04c5b0792e76afa89213c7f0ae55f485786b9
twesleyb <tyler.w.bradshaw@duke.edu>
2020-02-27 16:05:43 -0500

    add some checks to mapIDs function

--- R/mapIDs.R
+++ R/mapIDs.R
@@ -26,11 +26,11 @@
 #'
 #' @examples
 #' mapIds(mygenes, from = "symbol", to = "entrez", species = "mouse")
-mapIDs <- function(identifiers, from, to, species, ...) {
+mapIDs <- function(identifiers, from, to, species, multiVals="first", ...) {
   # Wrapper around AnnotationDbi::mapIds()
   # Note: MGI ids should be in the following format: "MGI:MGI:3649456"
   # Imports.
-  require(getPPIs)
+  #require(getPPIs)
   # Get organism specific mapping database.
   annotationDBs <- mappingDBs()
   orgDB <- unlist(annotationDBs[sapply(annotationDBs, "[", 3) == tolower(species)])
@@ -39,17 +39,35 @@ mapIDs <- function(identifiers, from, to, species, ...) {
     eval(parse(text = paste0("require(", orgDB[["database"]], ",quietly=TRUE)")))
   })
   osDB <- eval(parse(text = orgDB[["database"]]))
-  # Map gene identifiers. Suppress messages.
+  # Get input type (from) and output type (to).
   colIDto <- grep(toupper(to), columns(osDB))
   colIDfrom <- grep(toupper(from), columns(osDB))
+  # Check that from and to map to a single column.
+  keys <- keytypes(osDB)
+  msg <- paste0("Please provide one of the following",
+		"(case insensitive): ", paste(keys,collapse=", "),".")
+  if (length(colIDto) > 1) { 
+	  stop(paste("Multiple matching keys (to).\n",msg)) 
+  }
+  if (length(colIDfrom) > 1) { 
+	  stop(paste("Multiple matching keys (from).\n",msg)) 
+  }
+  # Map gene identifiers.
   suppressMessages({
     output <- AnnotationDbi::mapIds(osDB,
       keys = as.character(identifiers),
       column = columns(osDB)[colIDto],
       keytype = columns(osDB)[colIDfrom],
-      multiVals = "first"
+      multiVals = multiVals
     )
   })
+  # Check if output is a list.
+  if (is.list(output)) {
+	  # Replace NULL
+	  is_null <- sapply(output,is.null)
+	  output[is_null] <- NA
+	  output <- unlist(output)
+  }
   # Check that all nodes (entrez) are mapped to gene symbols.
   not_mapped <- is.na(output)
   if (sum(is.na(output)) != 0) {
