1964a98db35152d217494c54f8622e389ac7aad7
twesleyb <twesleyb10@gmail.com>
2020-12-22 08:54:35 -0500

    lazy git commit

---
+++ R/hsInteractome_data.R
@@ -0,0 +1,16 @@
+#' hsInteractome data
+#'
+#' @docType data
+#'
+#' @usage data("hsInteractome")
+#'
+#' @format An object of class \code{"data.frame"}.
+#'
+#' @keywords datasets
+#'
+#' @references HitPredict
+#'
+#' @source \href{}{}
+#'
+#' @examples
+#' data("hsInteractome") # load HitPredict human PPIs
---
+++ R/musInteractome_data.R
@@ -0,0 +1,16 @@
+#' musInteractome data
+#'
+#' @docType data
+#'
+#' @usage data("musInteractome")
+#'
+#' @format An object of class \code{"data.frame"}.
+#'
+#' @keywords datasets
+#'
+#' @references HitPredict
+#'
+#' @source \href{}{}
+#'
+#' @examples
+#' data("musInteractome") # load HitPredict (human) PPIs mapped to mouse entrez
